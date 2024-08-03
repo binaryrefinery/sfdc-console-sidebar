7a9e8f7c76ac6398cbac3fa0fe6a29600773af48
Michael Sluis <msluis@me.com>
2024-08-03 15:01:46 +1000

    Final Update for Chrome Plugin Listing
    Added images and updated manifest

--- README.md
+++
@@ -1,2 +0,0 @@
-# sfdc-console-sidebar
-Changes the Salesforce.com Developer Console top tab bar into a sidebar using pure css
--- update-title.js
+++ update-title.js
@@ -1,4 +1,4 @@
 let orgName = window.location.hostname.split('.')[0];
-let base = '## Dev Console: ';
+let base = 'Dev:';
 let titleElement = document.querySelector('title');
 titleElement.innerText = base + ' ' + orgName.toUpperCase();
